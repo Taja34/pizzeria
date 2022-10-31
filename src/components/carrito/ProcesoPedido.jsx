95215e2132b6d4a1a030a7312e891d76172a3824
Santiago <santi.gutierrez.restrepo@gmail.com>
2022-10-31 09:38:23 -0500

    first commit

---
+++ src/components/carrito/ProcesoPedido.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProcesoPedido = () => {
+  return (
+    <div>ProcesoPedido</div>
+  )
+}
+
+export default ProcesoPedido
---
+++ src/components/routes/Router.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Carrito from "../carrito/Carrito";
+import Home from "../Home/Home";
+import Login from "../sesion/Login";
+import Register from "../sesion/Register";
+
+
+
+const Router = () => {
+   
+    return ( 
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register"  element={<Register />} />
+                <Route path="carrito"  element={<Carrito />} />
+                
+            </Routes>
+        </BrowserRouter>
+   
+    )
+}
+
+export default Router;
